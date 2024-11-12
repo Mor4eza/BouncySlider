23f1f1a61616891bd960ac9b87f2b80bb9b5faa2
Morteza Gharedaghi <M.Gharedaghi@sadad.co.ir>
2024-11-12 14:49:47 +0330

    use BouncySlider in example project

--- BouncySlider_example/ContentView.swift
+++ BouncySlider_example/ContentView.swift
@@ -6,16 +6,29 @@
 //
 
 import SwiftUI
+import BouncySlider
 
 struct ContentView: View {
+
+    @State var value: CGFloat = 0.5
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            BouncySlider(value: $value, orientation: .vertical) {
+                if value == 1.0 {
+                    Image(systemName: "speaker.wave.3.fill").foregroundStyle(.gray)
+                } else if value < 1.0 && value > 0.36 {
+                    Image(systemName: "speaker.wave.2.fill").foregroundStyle(.gray)
+                } else if value <= 0.35 && value > 0.0 {
+                    Image(systemName: "speaker.wave.1.fill").foregroundStyle(.gray)
+                } else {
+                    Image(systemName: "speaker.fill").foregroundStyle(.gray)
+                }
+                
+            }
+            Text("Volume: \(Int(value * 100))%")
+                
         }
-        .padding()
+
     }
 }
 
