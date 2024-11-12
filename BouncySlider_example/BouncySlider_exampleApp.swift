e478cea961ba9e0638b0c3c364f083dbd4ddaff6
Morteza Gharedaghi <M.Gharedaghi@sadad.co.ir>
2024-11-12 11:11:51 +0330

    Initial Commit

---
+++ BouncySlider_example/BouncySlider_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BouncySlider_exampleApp.swift
+//  BouncySlider_example
+//
+//  Created by Morteza on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BouncySlider_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
